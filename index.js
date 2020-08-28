dd475388fb636f29c86c98ec8c7b1e2c95a1ab7d
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-08-29 00:23:10 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { exec } = require("child_process");
+const path = require("path");
+
+const projectDir = path.resolve(__dirname, '/../../');
+const pkg = require(projectDir + 'package.json')
+const tmpDir = projectDir + '/tmp';
+const codeDir = path.resolve(projectDir, pkg.ghPagesDeploy.directory);
+
+const runCmd = (cmd) => {
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      //return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      //return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+const shellScript = ` rm -rf ${codeDir}/.git ${tmpDir} && \\
+  mkdir -p ${tmpDir} && \\
+  cp -R ${codeDir} ${tmpDir}/build && \\
+  git clone -b gh-pages ${pkg.ghPagesDeploy.repository} ${tmpDir}/gh-pages && \\
+  mv ${tmpDir}/gh-pages/.git ${codeDir}/.git && \\
+  cd ${codeDir} && \\
+  git add -A && \\
+  git commit -m "${pkg.ghPagesDeploy.commitMessage}" && \\
+  rm -rf ${tmpDir} && \
+  cd ${codeDir} && \
+  git push
+`
+
+runCmd(shellScript);
